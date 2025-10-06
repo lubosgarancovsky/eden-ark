b67d3fb1b34a47ebbe23b1331e4319f719b0a862
Ľuboš Garančovský <lubos.garancovsky@gmail.com>
2025-10-06 22:08:21 +0200

    vite

---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from "react-router";
+import {LoginPage, CreateAccountPage, ForgotPasswordPage, ResetPasswordPage} from "./pages";
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <BrowserRouter>
+         <Routes>
+             <Route path="/login" element={ <LoginPage />}/>
+             <Route path="/create-account" element={ <CreateAccountPage />}/>
+             <Route path="/forgot-password" element={ <ForgotPasswordPage />}/>
+             <Route path="/reset-password" element={ <ResetPasswordPage />}/>
+         </Routes>
+      </BrowserRouter>
+  </StrictMode>,
+)
